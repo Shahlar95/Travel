c6c1b843bc34ad49f417b208e901b09f3f808a25
unknown <asahlar3@gmail.com>
2023-11-29 18:38:07 +0400

    add/react/bostrap

--- travel/src/App.js
+++ travel/src/App.js
@@ -1,9 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
+import Navbar from './layout/navbar/navbar';
 import './App.css';
+import './scss/style.css';
+
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
       
     </div>
   );
