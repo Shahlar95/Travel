b5b51fb6bd2a03b98af8d4667581e272d7c0ed52
unknown <asahlar3@gmail.com>
2023-12-01 11:01:42 +0400

    add/navbar/submenu/add/before

--- travel/src/page/home/home.jsx
+++ travel/src/page/home/home.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 function Home() {
   return (
-    <div>Home</div>
-  )
+    <div>
+      <section
+        className='where'
+        style={{
+          backgroundImage: 'url("https://s3-alpha-sig.figma.com/img/fccf/d5fa/bb1cf41c431c855d2e813fd554b5e09f?Expires=1702252800&Signature=c5hlKD3m34hQnzoX3sAb0e8p48E94alIZtSgQRzyi8F1Fn4y8ObQcg6k8DfI0SOeRelkz4Q9GSr7Onvcafwas6G44FcFIEcsrHa7zMf06Kw1OWe5tHa3m5CIixekOAI7iQBgwJdUwhqSLLRZl83H4P6QHg4L48tIsqhbUGV943XsiRp3IfRyU44eRpmdQf1eDybZyZvlx~vQEDbFCC8a~Pj2ZvrorQ4jfV3QYEwS-u4fctzeHvp~ESA0WBm5Ex~~JZBpjFTr3LMv6HFLuYz1n3suspOmBSyQlpTJvYFWL4F6ffzrgISAgUGqVHvCd-slkJbh4ywBpOhphXIwKFA25Q__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4")',
+        }}
+      >
+        
+      </section>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
